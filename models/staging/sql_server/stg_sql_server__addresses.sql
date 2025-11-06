96aa33b4d4906c6ce9d8beb629f691a8e4fc1c5a
Paula del Moral <pdelmoralmilla@gmail.com>
2025-11-06 16:53:20 +0000

    Bronze source and addresses, orders, products first modeling

---
+++ models/staging/sql_server/stg_sql_server__addresses.sql
@@ -0,0 +1,7 @@
+SELECT
+    ADDRESS_ID,
+    IFNULL(ZIPCODE, 'Unkown') AS ZIPCODE,
+    TRIM(COUNTRY,' ') AS COUNTRY,
+    TRIM(ADDRESS,' ') AS ADDRESS,
+    TRIM(STATE,' ') AS STATE    
+FROM {{source("sql_server", 'ADDRESSES')}}
